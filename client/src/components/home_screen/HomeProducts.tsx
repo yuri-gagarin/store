b9a595beeaadec6624c6653d57aba5e9f5a9234b
Yuriy Ivanov <ymivanov@icloud.com>
2020-05-14 17:05:14 +0300

    home screen products styling

--- client/src/components/home_screen/HomeProducts.tsx
+++ client/src/components/home_screen/HomeProducts.tsx
@@ -1,27 +1,112 @@
-import React from "react";
-import { Grid, Segment, Image } from "semantic-ui-react";
+import React, { useState, useRef, useEffect } from "react";
+import { Grid, Segment, Image, Icon, Button } from "semantic-ui-react";
 // css imports //
 import "./css/homeProducts.css";
 
+interface ElementStatus {
+  firstVisible: boolean;
+  secondVisible: boolean;
+  thirdVisible: boolean;
+};
+
 const HomeProducts: React.FC<{}> = (props): JSX.Element => {
+  const initElStatus: ElementStatus = { firstVisible: false, secondVisible: false, thirdVisible: false }; 
+  // local state and refs //
+  const homeProductsRef = useRef<HTMLDivElement>(document.createElement("div"));
+  const [ elementStatus, setElementStatus ] = useState<ElementStatus>(initElStatus);
+  const [ showComp, setShowComp ] = useState<boolean>(false);
+
+  // scroll spy to watch if component is in view //
+  const scrollSpy = (e: Event): void => {
+    const compRect: DOMRect = homeProductsRef.current.getBoundingClientRect();
+    if (compRect.y + 100 < window.innerHeight) {
+      setShowComp(true);
+    }
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", scrollSpy, true);
+    return () => window.removeEventListener("scroll", scrollSpy);
+  }, [homeProductsRef]);
+  // will fade in each element one by one //
+  useEffect(() => {
+    if (showComp) {
+      setTimeout(() => {
+        setElementStatus((state) => ({ ...state, firstVisible: true }));
+      }, 500);
+      setTimeout(() => {
+        setElementStatus((state) => ({ ...state, secondVisible: true }));
+      }, 1000);
+      setTimeout(() => {
+        setElementStatus((state) => ({ ...state, thirdVisible: true }));
+      }, 1500);
+      window.removeEventListener("scroll", scrollSpy);
+    }
+  }, [showComp]);
+
   return (
-    <div className="homeProductsScreenComp">
+    <div className="homeProductsHolder" ref={homeProductsRef}>
       <Grid stackable container columns={3}>
-        <Grid.Column>
-          <Segment style={{ border: "2px solid red", height: "300px" }}>
-            <Image src="/images/home_page/stock_store1.jpeg"></Image>
-          </Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment style={{ border: "2px solid red", height: "300px" }}>
-            <Image src="/images/home_page/stock_store1.jpeg"></Image>
-          </Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment style={{ border: "2px solid red", height: "300px" }}>
-            <Image src="/images/home_page/stock_store1.jpeg"></Image>
-          </Segment>
-        </Grid.Column>
+        <Grid.Row>
+            <h3 className="homeProductsHeader">Products Showcase</h3>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column className={ elementStatus.firstVisible ? "homeProdSegHolder showHomeProd" : "homeProdSegHolder" }>
+            <Segment className="homeProductSegment">
+              <Image src="/images/home_page/stock_store1.jpeg"></Image>
+              <div className="homeProductImgCover">
+                <Icon name="search plus" className="homeProductMoreIcon"></Icon>
+              </div>
+            </Segment>
+            <Segment className="homeProductDesc">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's..   
+              </p>
+              <Button basic color="olive" className="productMoreBtn">
+                More...
+              </Button>
+            </Segment>
+        
+          </Grid.Column>
+          <Grid.Column className={ elementStatus.secondVisible ? "homeProdSegHolder showHomeProd" : "homeProdSegHolder" }>
+            <Segment className="homeProductSegment">
+              <Image src="/images/home_page/stock_store1.jpeg"></Image>
+              <div className="homeProductImgCover">
+                <Icon name="search plus" className="homeProductMoreIcon"></Icon>
+              </div>
+            </Segment>
+            <Segment className="homeProductDesc">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's..   
+              </p>
+              <Button basic color="olive" className="productMoreBtn">
+                More...
+              </Button>
+            </Segment>
+        
+          </Grid.Column>
+          <Grid.Column className={ elementStatus.thirdVisible ? "homeProdSegHolder showHomeProd" : "homeProdSegHolder" }>
+            <Segment className="homeProductSegment">
+              <Image src="/images/home_page/stock_store1.jpeg"></Image>
+              <div className="homeProductImgCover">
+                <Icon name="search plus" className="homeProductMoreIcon"></Icon>
+              </div>
+            </Segment>
+            <Segment className="homeProductDesc">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industry's..   
+              </p>
+              <Button basic color="olive" className="productMoreBtn">
+                More...
+              </Button>
+            </Segment>
+        
+          </Grid.Column>
+
+        </Grid.Row>
+       
       </Grid>
     </div>
   );
--- client/src/components/home_screen/HomeScreenComponent.tsx
+++ client/src/components/home_screen/HomeScreenComponent.tsx
@@ -5,6 +5,7 @@ import HomeHeader from "./HomeHeader";
 import HomeProducts from "./HomeProducts";
 import HomeStore from "./HomeStore";
 // css imports //
+import "./css/homeScreenComponent.css";
 
 interface Props {
   title: string
@@ -13,6 +14,7 @@ const HomeScreenComponent: React.FC<Props> = (props): JSX.Element => {
   return (
     <React.Fragment>
         <HomeHeader />
+        <div className="homeScreenParallax1"></div>
         <HomeProducts />
         <HomeStore />
     </React.Fragment>
