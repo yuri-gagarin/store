b41700e87584e0dae44c14efb652f039eec23154
Yuriy Ivanov <ymivanov@icloud.com>
2020-05-15 20:24:51 +0300

    store header and basic styling

--- client/src/components/client_components/store_screen/StoreHolder.tsx
+++ client/src/components/client_components/store_screen/StoreHolder.tsx
@@ -1,27 +1,64 @@
 import React from "react";
 import { Grid } from "semantic-ui-react";
+// additional components //
+import ServiceComponent from "./services/ServiceComponent";
+import FooterBar from "../navbars/FooterBar";
 // css imports //
 import "./css/storeHolder.css";
+
+// mock services, later from db //
+const services = [
+  {
+    description: "service 1"
+  },
+  {
+    description: "service 2"
+  },
+  {
+    description: "service 3"
+  },
+  {
+    description: "service 4"
+  },
+  {
+    description: "service 5"
+  }
+];
 
 const StoreHolder: React.FC<{}> = (props): JSX.Element => {
   return (
     <div className="storeHolderDiv">
-      <Grid columns={1}>
+      <div id="storeTitle">
+        <p>Store</p>
+        <p>and</p>
+        <p>Services</p>
+      </div>
+      
+      <Grid columns={1} centered>
         <Grid.Row>
-          <Grid.Column mobile={16} tablet={12} computer={14}>
-
+          <Grid.Column mobile={16} tablet={16} computer={16}>
+            <div className="storeHeader">
+              <div className="storeGallery">
+                {
+                  services.map((service) => <ServiceComponent description={service.description}/>)
+                }
+              </div>
+            </div>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
-          <Grid.Column>
-
+          <Grid.Column mobile={16} tablet={16} computer={16}>
+            
           </Grid.Column>
         </Grid.Row><Grid.Row>
-          <Grid.Column>
-
+          <Grid.Column mobile={16} tablet={16} computer={16}>
+            <div className="storeDetails">
+            
+            </div>
           </Grid.Column>
         </Grid.Row>
       </Grid>
+      <FooterBar />
     </div>
   );
 };
---
+++ client/src/components/client_components/store_screen/services/ServiceComponent.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { Image, Icon } from "semantic-ui-react";
+// css imports //
+import "./css/storeServices.css";
+
+interface Props {
+  coverUrl?: string;
+  pictureUrl?: string;
+  description?: string;
+};
+
+const ServiceComponent: React.FC<Props> = ({ coverUrl, pictureUrl, description }): JSX.Element => {
+  const [ covUrl, setCoverUrl ] = useState<string>("");
+  const [ picUrl, setPicUrl ] = useState<string>("");
+
+  const pullCoverUrl = () => {
+    if (coverUrl) {
+      setCoverUrl(coverUrl);
+    } else {
+      setCoverUrl("/images/services/service2.jpeg");
+    }
+  };
+  const pullPicUrl = () => {
+    if (pictureUrl) {
+      setPicUrl(pictureUrl);
+    } else {
+      setPicUrl("/images/services/service1.jpeg");
+    }
+  };
+  // set image url or default image //
+  useEffect(() => {
+    pullPicUrl();
+    pullCoverUrl();
+  }, []);
+  return (
+    <div className="storeServiceHolder">
+      <div className="storeServiceHiddenImg">
+        <Image src={picUrl} size="medium"  />
+      </div>
+      <div className="storeServiceCoverImg" >
+        <Image src={covUrl} size="medium" />
+      </div>
+      <Icon name="plus"></Icon>
+
+    </div>
+
+    
+  );
+};
+
+export default ServiceComponent;
