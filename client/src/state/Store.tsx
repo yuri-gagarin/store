afc19e92fbb0a27e79a70ee6b8d45b60eb0a8894
Yuriy Ivanov <ymivanov@icloud.com>
2020-05-25 16:36:29 +0300

    simple state logger

--- client/src/state/Store.tsx
+++ client/src/state/Store.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer, useEffect } from "react";
+import React, { createContext, useReducer, useEffect, useRef } from "react";
 import { indexReducer, rootState } from "./reducers/indexReducer";
 import { initialStoreState } from "./reducers/storeReducer";
 // global app state //
@@ -24,14 +24,27 @@ export const Store = createContext<IGlobalAppContext>(initialContext);
 const logStateChanges = (state: IGlobalAppState): void => {
   console.log("state changed");
   for (const key in state) {
+    console.log(key);
     console.log(state[key]);
   }
 }
 export const StateProvider: React.FC<{}> = ({ children }): JSX.Element => {
   const [ globalState, dispatch ] = useReducer(indexReducer, rootState);
-  useEffect(() => {
-    logStateChanges(globalState);
-  }, [globalState])
+
+  const usePrevious = (state: IGlobalAppState) => {
+    const ref = useRef<IGlobalAppState>();
+    useEffect(() => {
+      ref.current = state;
+      console.log("Previous State");
+      console.log(ref.current);
+      console.log("Current State");
+      console.log(state);
+    }, [state])
+   
+  };
+
+  usePrevious(globalState)
+
   return  (
     <Store.Provider value={{ state: globalState, dispatch: dispatch }}>
       {children}
