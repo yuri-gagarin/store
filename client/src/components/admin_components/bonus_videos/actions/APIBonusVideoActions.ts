5edbefc9ac1a36f43b3ee7120f05fa8fdf219016
Yuriy Ivanov <ymivanov@icloud.com>
2020-07-08 18:02:21 +0300

    bonus video api actions

---
+++ client/src/components/admin_components/bonus_videos/actions/APIBonusVideoActions.ts
@@ -0,0 +1,180 @@
+import axios, { AxiosRequestConfig, AxiosError } from "axios";
+import { Dispatch } from "react";
+import { IGlobalAppState } from "../../../../state/Store";
+import { AppAction } from "../../../../state/Store";
+
+interface IBonusVideoServerResData {
+  responseMsg: string;
+  bonusVideo?: IBonusVideoData;
+  newBonusVideo?: IBonusVideoData;
+  editedBonusVideo?: IBonusVideoData;
+  deletedBonusVideo?: IBonusVideoData;
+  bonusVideos?: IBonusVideoData[];
+}
+interface IBonusVideoServerRes {
+  data: IBonusVideoServerResData
+}
+
+type NewBonusVideoData = {
+  description: string;
+  youTubeURL: string;
+  vimeoURL: string;
+}
+type EditedBonusVideoData = NewBonusVideoData;
+
+export const getAllBonusVideos = (dispatch: Dispatch<BonusVideoAction>): Promise<boolean> => {
+  const requestOptions: AxiosRequestConfig = {
+    method: "get",
+    url: "/api/bonus_videos"
+  };
+  return axios.request<IBonusVideoServerResData, IBonusVideoServerRes>(requestOptions)
+    .then((response) => {
+      const { data } = response;
+      const bonusVideos = data.bonusVideos;
+      dispatch({ type: "GET_ALL_BONUS_VIDEOS", payload: {
+        loading: false,
+        responseMsg: data.responseMsg,
+        loadedBonusVideos: bonusVideos,
+        error: null
+      }});
+      return true;
+    })
+    .catch((error: AxiosError) => {
+      dispatch({ type: "SET_BONUS_VIDEO_ERROR", payload: {
+        loading: false,
+        responseMsg: error.message,
+        error: error
+      }});
+      return false;
+    });
+};
+
+export const getBonusVideo = (_id: string, dispatch: Dispatch<BonusVideoAction>): Promise<boolean> => {
+  const requestOptions: AxiosRequestConfig = {
+    method: "get",
+    url: "/api/bonus_videos/" + _id,
+  };
+  return axios.request<IBonusVideoServerResData, IBonusVideoServerRes>(requestOptions)
+    .then((response) => {
+      const { data } = response;
+      const bonusVideo  = data.bonusVideo;
+      dispatch({ type: "GET_BONUS_VIDEO", payload: {
+        loading: false,
+        responseMsg: data.responseMsg,
+        currentBonusVideoData: bonusVideo,
+        error: null
+      }});
+      return true;
+    })
+    .catch((error: AxiosError) => {
+      dispatch({ type: "SET_BONUS_VIDEO_ERROR", payload: {
+        loading: false,
+        responseMsg: error.message,
+        error: error
+      }});
+      return false;
+    });
+};
+
+export const createBonusVideo = ({ description, youTubeURL, vimeoURL }: NewBonusVideoData, dispatch: Dispatch<BonusVideoAction>): Promise<boolean> => {
+  const requestOptions: AxiosRequestConfig = {
+    method: "post",
+    url: "/api/bonus_videos/create",
+    data: {
+      description: description,
+      youTubeURL: youTubeURL,
+      vimeoURL: vimeoURL
+    }
+  };
+
+  return axios.request<IBonusVideoServerResData, IBonusVideoServerRes>(requestOptions)
+    .then((response) => {
+      const { data } = response;
+      const newBonusVideo = data.newBonusVideo;
+      dispatch({ type: "CREATE_BONUS_VIDEO", payload: {
+        loading: false,
+        responseMsg: data.responseMsg,
+        newBonusVideo: newBonusVideo,
+        error: null
+      }});
+      return true;
+    })
+    .catch((error: AxiosError) => {
+      console.error(error)
+      dispatch({ type: "SET_BONUS_VIDEO_ERROR", payload: {
+        loading: false,
+        responseMsg: "An Error occured",
+        error: error
+      }});
+      return false;
+    });
+};
+
+export const editBonusVideo = (_id: string, data: EditedBonusVideoData, dispatch: Dispatch<BonusVideoAction>, state: IGlobalAppState) => {
+  const { loadedBonusVideos } = state.bonusVideoState;
+  const requestOptions: AxiosRequestConfig = {
+    method: "patch",
+    url: "/api/bonus_videos/update/" + _id,
+    data: {
+      ...data,
+    }
+  };
+  return axios.request<IBonusVideoServerResData, IBonusVideoServerRes>(requestOptions)
+    .then((response) => {
+      const { data } = response;
+      const editedBonusVideo = data.editedBonusVideo;
+      const updatedBonusVideos = loadedBonusVideos.map((video) => {
+        if (vide._id === editedBonusVideo._id) {
+          return editedBonusVideo;
+        } else {
+          return video;
+        }
+      });
+      dispatch({ type: "EDIT_BONUS_VIDEO", payload: {
+        loading: false,
+        responseMsg: data.responseMsg,
+        loadedBonusVideos: updatedBonusVideos,
+        error: null
+      }});
+      return true;
+    })
+    .catch((error: AxiosError) => {
+      dispatch({ type: "SET_BONUS_VIDEO_ERROR", payload: {
+        loading: false,
+        responseMsg: "An error occured",
+        error: error
+      }});
+    });
+};
+
+export const deleteBonusVideo = (_id: string, dispatch: Dispatch<BonusVideoAction>, state: IGlobalAppState): Promise<boolean> => {
+  const { loadedBonusVideos } = state.bonusVideoState;
+  const requestOptions: AxiosRequestConfig = {
+    method: "delete",
+    url: "/api/bonus_videos/delete/" + _id,
+  };
+  return axios.request<IBonusVideoServerResData, IBonusVideoServerRes>(requestOptions)
+    .then((response) => {
+      const { data } = response;
+      const deletedBonusVideo = data.deletedBonusVideo;
+      const updatedBonusVideos = loadedBonusVideos.filter((video) => {
+        return video._id !== deletedBonusVideo._id;
+      })
+
+      dispatch({ type: "DELETE_BONUS_VIDEO", payload: {
+        loading: false,
+        responseMsg: data.responseMsg,
+        loadedBonusVideos: updatedBonusVideos,
+        error: null
+      }});
+      return true;
+    })
+    .catch((error: AxiosError) => {
+      dispatch({ type: "SET_BONUS_VIDEO_ERROR", payload: {
+        loading: false,
+        responseMsg: "A delete error occured",
+        error: error
+      }});
+      return false;
+    });
+};
---
+++ client/src/custom_types/bonusVideoTypes.d.ts
@@ -0,0 +1,96 @@
+type GetAllBonusVideos = {
+  readonly type: "GET_ALL_BONUS_VIDEOS";
+  readonly payload: {
+    loading: boolean;
+    responseMsg: string;
+    loadedBonusVideos: IBonusVideoData[];
+    error: null | Error;
+  };
+}
+type SetCurrentBonusVideo = {
+  readonly type: "SET_CURRENT_BONUS_VIDEO";
+  payload: {
+    currentBonusVideoData: IBonusVideoData;
+  }
+}
+type ClearCurrentBonusVideo = {
+  readonly type: "CLEAR_CURRENT_BONUS_VIDEO";
+  payload: null;
+}
+type GetBonusVideo = {
+  readonly type: "GET_BONUS_VIDEO";
+  readonly payload: {
+    loading: boolean;
+    responseMsg: string;
+    currentBonusVideoData: IBonusVideoData;
+    error: null | Error;
+  };
+}
+type CreateBonusVideo = {
+  readonly type: "CREATE_BONUS_VIDEO";
+  readonly payload: {
+    loading: boolean;
+    responseMsg: string;
+    newBonusVideo: IBonusVideoData;
+    error: null | Error;
+  };
+} 
+type EditBonusVideo = {
+  readonly type: "EDIT_BONUS_VIDEO";
+  readonly payload: {
+    loading: boolean;
+    responseMsg: string;
+    editedBonusVideo: IBonusVideoData;
+    loadedBonusVideos: IBonusVideoData[];
+    error: null | Error;
+  };
+}
+type UpdateLoadedBonusVideos = {
+  readonly type: "UPDATE_LOADED_BONUS_VIDEOS";
+  readonly payload: {
+    loadedBonusVideos: IBonusVideoData[];
+  }
+}
+type DeleteBonusVideo = {
+  readonly type: "DELETE_BONUS_VIDEO";
+  readonly payload: {
+    loading: boolean;
+    responseMsg: string;
+    loadedBonusVideos: IBonusVideoData[];
+    error: null | Error;
+  };
+}
+type SetBonusVideoError = {
+  readonly type: "SET_BONUS_VIDEO_ERROR";
+  readonly payload: {
+    loading: boolean;
+    responseMsg: string;
+    error: Error;
+  };
+}
+type ClearBonusVideoError = {
+  readonly type: "ClEAR_BONUS_VIDEO_ERROR";
+  readonly payload: {
+    loading: boolean;
+    responseMsg: string;
+    error: null;
+  };
+}
+
+declare interface IBonusVideoData {
+  _id: string;
+  description: string;
+  youTubeURL: string;
+  vimeoURL: string;
+  createdAt: string;
+  editedAt?: string;
+}
+declare interface IBonusVideoState {
+  loading: boolean;
+  responseMsg: string;
+  currentBonusVideoData: IBonusVideoData;
+  loadedBonusVideos: IBonusVideoData[]
+  error: null | Error;
+}
+declare type BonusVideoAction = GetAllBonusVideos | GetBonusVideo | SetCurrentBonusVideo | ClearCurrentBonusVideo | CreateBonusVideo | EditBonusVideo | 
+                          DeleteBonusVideo | SetBonusVideoError | ClearBonusVideoError | UploadNewBonusVideoImg | UpdateBonusVideoImgs | DeleteBonusVideoImg;
