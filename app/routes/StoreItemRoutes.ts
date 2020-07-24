4b193ac7c03f2ef1d8d67811684b2c805e82659b
Yuriy Ivanov <ymivanov@icloud.com>
2020-07-24 20:43:45 +0300

    implement StoreItems routes

---
+++ app/routes/StoreItemRoutes.ts
@@ -0,0 +1,40 @@
+import { Router} from "express";
+import { RouteConstructor } from "./helpers/routeInterfaces";
+import { IGenericController } from "../controllers/helpers/controllerInterfaces";
+
+class StoreItemRoutes extends RouteConstructor<IGenericController> {
+  private viewAllStoreItemsRoute = "/api/store_items";
+  private viewStoreItemRoute = "/api/store_items/:_id";
+  private createStoreItemRoute = "/api/store_items/create";
+  private editStoreItemRoute = "/api/store_items/update/:_id";
+  private deleteStoreItemRoute = "/api/store_items/delete/:_id";
+  
+  constructor (router: Router, controller: IGenericController) {
+    super(router, controller);
+    this.initializeRoutes();
+  }
+  protected initializeRoutes (): void {
+    this.getAllStoreItems();
+    this.getStoreItem();
+    this.createStoreItem();
+    this.editStoreItem();
+    this.deleteStoreItem();
+  }
+  private getAllStoreItems (): void {
+    this.Router.route(this.viewAllStoreItemsRoute).get(this.controller.index!);
+  }
+  private getStoreItem (): void {
+    this.Router.route(this.viewStoreItemRoute).get(this.controller.get);
+  }
+  private createStoreItem (): void {
+    this.Router.route(this.createStoreItemRoute).post(this.controller.create);
+  }
+  private editStoreItem (): void {
+    this.Router.route(this.editStoreItemRoute).patch(this.controller.edit);
+  }
+  private deleteStoreItem (): void {
+    this.Router.route(this.deleteStoreItemRoute).delete(this.controller.delete);
+  }
+}
+
+export default StoreItemRoutes;
