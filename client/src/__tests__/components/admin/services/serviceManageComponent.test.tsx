cbbe03355828ba478427b05cc5fb365ea6afd99f
Yuriy Ivanov <ymivanov@icloud.com>
2020-09-12 15:21:25 +0300

    setup for ServiceManageHolder render tests

--- client/src/__tests__/components/admin/services/serviceManageComponent.test.tsx
+++ client/src/__tests__/components/admin/services/serviceManageComponent.test.tsx
@@ -1,225 +1,205 @@
-import React, { useReducer, useEffect } from "react";
-import { mount, ReactWrapper, shallow } from "enzyme";
+import React from "react";
+import { mount, ReactWrapper } from "enzyme";
 import moxios from "moxios";
 import { MemoryRouter as Router } from "react-router-dom";
 // components //
-import { act } from "react-dom/test-utils";
 import ServiceManageHolder from "../../../../components/admin_components/services/service_manage/ServiceManageHolder";
-import LoadingScreen  from "../../../../components/admin_components/miscelaneous/LoadingScreen";
+import LoadingScreen from "../../../../components/admin_components/miscelaneous/LoadingScreen";
+import ErrorComponent from "../../../../components/admin_components/miscelaneous/ErrorScreen";
+import { act } from "react-dom/test-utils";
 import { createMockServices } from "../../../../test_helpers/serviceHelpers";
-import { generateCleanState } from "../../../../test_helpers/miscHelpers";
-import { initialContext, StateProvider, IGlobalAppState } from "../../../../state/Store";
+import { StateProvider } from "../../../../state/Store";
 import ServiceCard from "../../../../components/admin_components/services/service_manage/ServiceCard";
-import { indexReducer, rootState } from "../../../../state/reducers/indexReducer";
-import ServiceComponent from "../../../../components/client_components/store_screen/services/ServiceComponent";
-import { Grid } from "semantic-ui-react";
+import ErrorScreen from "../../../../components/admin_components/miscelaneous/ErrorScreen";
+import { error } from "console";
 
 describe("Service Manage Holder Tests", () => {
-  beforeEach(() => {
-    moxios.install();
-  });
-  afterEach(() => {
-    moxios.uninstall();
-  });
-  describe("Default Component state at first render", () => {
-    let component: ReactWrapper; let loadingScreen: ReactWrapper;
-    let state: IGlobalAppState; 
+  
+    describe("Default Component state at first render", () => {
+      let component: ReactWrapper; let loadingScreen: ReactWrapper;
+      beforeAll( async () => {
+        moxios.install();
+        component = mount(
+          <Router>
+            <StateProvider>
+              <ServiceManageHolder />
+            </StateProvider>
+          </Router>
+        );
 
-    beforeAll(() => {
-      state = generateCleanState();
-      component = mount(
-        <Router>
-          <StateProvider>
-            <ServiceManageHolder state={state} dispatch={jest.fn}/>
-          </StateProvider>
-        </Router>
-      )
-    });
-    it("Should correctly render", () => {
-      expect(component).toMatchSnapshot();
-    });
-    it("Should render a 'LoadingScreen' Component before an API call resolves", () => {
-      loadingScreen = component.find(LoadingScreen);
-      expect(loadingScreen.length).toEqual(1)
-    });
-  });
-
-  describe("State after a successful API call", () => {
-    let component: ReactWrapper; let state: IGlobalAppState;
-    let services: IServiceData[];
-
-
-    beforeAll( async () => {
-      moxios.install();
-      state = generateCleanState();
-      services = createMockServices(5);
-
-      component = mount(
-        <Router>
-          <StateProvider>
-            <ServiceManageHolder state={state} dispatch={jest.fn} />
-
-          </StateProvider>
-        </Router>
-      );
-      await act( async () => {
-        moxios.stubRequest('/api/services/', {
-          status: 200,
-          responseText: 'hello',
-          response: {
-            responseMsg: "All ok",
-            services: services
-          }
+        await act( async () => {
+          await moxios.stubRequest("/api/services/", {
+            status: 200,
+            response: {
+              responseMsg: "All Ok",
+              services: []
+            }
+          });
         });
+
+      });
+       afterAll(() => {
+        moxios.uninstall();
+      }); 
+      
+      it("Should correctly render", () => {
+        expect(component).toMatchSnapshot();
       });
       
-      /*
-      await moxios.wait(jest.fn);
-      await act(async() => {
-        const request = moxios.requests.mostRecent();
-        await request.respondWith({
-          response: {
-            responseMsg: "All ok",
-            services: services
-          }
-        })
+      it("Should render a 'LoadingScreen' Component before an API call resolves", () => {
+        loadingScreen = component.find(LoadingScreen);
+        expect(loadingScreen.length).toEqual(1)
       });
-      */
     });
-
-    afterAll(() => {
-      moxios.uninstall();
-    })
     
-    it('Should correctly render initial Loading Screen', () => {
-      let loadingScreen = component.find(LoadingScreen);
-      expect(loadingScreen.length).toEqual(1);
-    });
-
-    it('Should NOT render initial Loading Screen after successful', () => {
-      act(() => {
-        component.update();
-      });
-      let loadingScreen = component.find(LoadingScreen);
-      expect(loadingScreen.length).toEqual(0);
-    });
-
-    it("Should render correct ServiceManageHolder Component", () => {
-      const serviceManageHolderComp = component.find("#serviceManageHolder");
-      expect(serviceManageHolderComp.at(0)).toBeDefined();
-      expect(serviceManageHolderComp.at(0)).toMatchSnapshot();
-    });
-
-    it("Should properly render all ServiceCard components", () => {
-      const comp = shallow(<ServiceManageHolder state={generateCleanState()} dispatch={jest.fn} />)
-      const globalState = generateCleanState();
-      const newState: IGlobalAppState = {
-        ...globalState,
-        serviceState: {
-          ...globalState.serviceState,
-          responseMsg: "All Ok",
-          loadedServices: [ ...services ]
-        }
-      }
-      // console.log(comp.find(Grid).length)
-      let f = 10;
-      let newr = comp.setProps({ state: newState})
-      newr.update()
-      console.log(newr.props())
-
-      /*
-      const serviceCards = component.find(ServiceCard);
-      expect(serviceCards.at(0)).toBeDefined();
-      expect(serviceCards.length).toEqual(services.length);
-      */
-    })
-  });
-});
-
-/*
+    // mock successful API call render tests //
     describe("State after a successful API call", () => {
       let component: ReactWrapper; let loadingScreen: ReactWrapper;
-      let Services: IServiceData[];
+      let services: IServiceData[];
 
-      beforeEach(() => {
+      beforeAll( async () => {
         moxios.install();
+        services = createMockServices(5);
+
+        component = mount(
+          <Router>
+            <StateProvider>
+              <ServiceManageHolder />
+            </StateProvider>
+          </Router>
+        );
+        
+        await act( async () => {
+          await moxios.stubRequest("/api/services/", {
+            status: 200,
+            response: {
+              responseMsg: "All Ok",
+              services: services
+            }
+          });
+        });
       });
-      afterEach(() => {
+
+      afterAll(() => {
         moxios.uninstall();
+      }); 
+
+      it("Should correctly render the initial Loading Screen", () => {
+        loadingScreen = component.find(LoadingScreen);
+        expect(loadingScreen.length).toEqual(1);
+      });
+      it("Should not render the initial Loading Screen after the API call", async () => {
+        act( () => {
+          component.update();
+        });
+        loadingScreen = component.find(LoadingScreen);
+        expect(loadingScreen.length).toEqual(0);
+      });
+      it("Should NOT render the ErrorScreen Component", () => {
+        const errorScreenComponent = component.find(ErrorScreen);
+        expect(errorScreenComponent.length).toEqual(0);
+      });
+  
+      it("Should render the correct ServiceManageHolder Component", () => {
+        const serviceManageHolderComp = component.find("#serviceManageHolder");
+        expect(serviceManageHolderComp.at(0)).toBeDefined();
+        expect(serviceManageHolderComp.at(0)).toMatchSnapshot();
+      });
+    
+      it("Should render correct number of ServiceCard components", () => {
+        const serviceCards = component.find(ServiceCard);
+        expect(serviceCards.length).toEqual(services.length);
       })
-      
-      it("Should fetch the request", async () => {
+    });
+    // END mock successfull API call render tests //
+    // mock ERROR API call render tests //
+    describe("State after a Error in API call", () => {
+      let component: ReactWrapper; let loadingScreen: ReactWrapper;
+      let services: IServiceData[];
+
+      beforeAll(async () => {
         await act(async () => {
-          await mount(
+          moxios.install();
+          component = await mount(
             <Router>
               <StateProvider>
                 <ServiceManageHolder />
-
               </StateProvider>
             </Router>
           );
-        });
-        expect(fetchMock).toHaveBeenCalledWith("thisurl")
-      });
-
-      it('Should display error if api request fail', async () => {
-      
-        let comp: ReactWrapper;
-        comp = mount(
-          <Router>
-            <StateProvider>
-              <ServiceManageHolder />
-
-            </StateProvider>
-          </Router>
-        );
-        await moxios.wait(jest.fn);
-        await act(async() => {
-          const request = moxios.requests.mostRecent()
-          await request.respondWith({
+          moxios.stubRequest("/api/services/", {
+            status: 500,
             response: {
-              responseMsg: "All ok",
-              Services: createMockServices(5)
+              responseMsg: "Error here",
+              error: new Error("API Call Error")
             }
-          })
+          });
         });
         act(() => {
-          comp.update();
+          moxios.uninstall();
         })
-        let ServiceCards = comp.find(ServiceCard)
-        console.log(ServiceCards.length)
       });
 
-      it('should display error if api request fail', async () => {
-      
-        let comp: ReactWrapper | null;
-        comp = mount(
-          <Router>
-            <StateProvider>
-              <ServiceManageHolder />
+      it("Should correctly render the initial Loading Screen", () => {
+        loadingScreen = component.find(LoadingScreen);
+        expect(loadingScreen.length).toEqual(1);
+      });
 
-            </StateProvider>
-          </Router>
-        );
-        await moxios.wait(jest.fn);
-        await act(async() => {
-          const request = moxios.requests.mostRecent()
-          await request.reject(new Error("Boo"));
+      it("Should not render the initial Loading Screen after an  API call", () => {
+        act( () => {
+          component.update();
         });
-        act(() => {
-          comp!.update();
-          console.log(comp?.props())
-        })
-    
-      });
-      /*
-      it("Should NOT render the 'LoadingScreen' Component", () => {
-        let loadingScreen = component.find(LoadingScreen);
+        loadingScreen = component.find(LoadingScreen);
         expect(loadingScreen.length).toEqual(0);
       });
-
-      it("Should render the Service Manage grid", () => {
-        //console.log(component.props())
-      })
+      it("Should render the ErrorScreen Component", () => {
+        const errorScreenComponent = component.find(ErrorScreen);
+        expect(errorScreenComponent.length).toEqual(1);
+      });
+      
+      it("Should NOT render the ServiceManageHolder Component", () => {
+        const serviceManageHolderComp = component.find("#serviceManageHolder");
+        expect(serviceManageHolderComp.length).toEqual(0);
+      });
+    
+      it("Should NOT render ANY ServiceCard components", () => {
+        const serviceCards = component.find(ServiceCard);
+        expect(serviceCards.length).toEqual(0);
+      });
+  
+      it("Should have a retry Service API call Button", () => {
+        const retryButton = component.find("#errorScreenRetryButton");
+        expect(retryButton.length).toEqual(2);
+      });
+      
+      
+      it("Should correctly re-dispatch the 'getServices' API request with the button click", async () => {
+        await act( async () => {
+          services = createMockServices(6);
+          moxios.install();
+          moxios.stubRequest("/api/services/", {
+            status: 200,
+            response: {
+              responseMsg: "All Ok",
+              services: services
+            }
+          });
+          const retryButton = component.find("#errorScreenRetryButton");
+          retryButton.at(0).simulate("click");
+        });
+        component.update()
+        const errorScreen = component.find(ErrorScreen);
+        const serviceManageHolderComp = component.find(ServiceManageHolder);
+        expect(errorScreen.length).toEqual(0);
+        expect(serviceManageHolderComp.length).toEqual(1);
+      });
+      
+      it("Should render correct number of 'ServiceCard' Components", () => {
+        const serviceCards = component.find(ServiceCard);
+        expect(serviceCards.length).toEqual(services.length);
+      });
     });
-    */
+    // END mock successfull API call tests //
+    
+  
+});
