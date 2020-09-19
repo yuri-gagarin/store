01d7c4adf132a864a8b443aa14d5f07866fda7c9
Yuriy Ivanov <ymivanov@icloud.com>
2020-09-19 14:54:42 +0300

    init setup for updated StoreFormHolder tests

--- client/src/__tests__/components/admin/stores/storeFormComponent.test.tsx
+++ client/src/__tests__/components/admin/stores/storeFormComponent.test.tsx
@@ -1,24 +1,34 @@
-import React from "react";
-import { mount, ReactWrapper } from "enzyme";
+import React from "react"
 import { Button } from "semantic-ui-react";
+// testing utils
+import { mount, ReactWrapper } from "enzyme";
+import moxios from "moxios";
+import { act } from "react-dom/test-utils";
+// client routing //
+import { MemoryRouter as Router } from "react-router-dom";
 // component imports //
 import StoreFormHolder from "../../../../components/admin_components/stores/forms/StoreFormHolder";
 import StoreForm from "../../../../components/admin_components/stores/forms/StoreForm";
-import StoreImageUplForm from "../../../../components/admin_components/stores/forms/StoreImageUplForm";
-import StoreImgPreviewHolder, { StoreImgPreviewThumb } from "../../../../components/admin_components/stores/image_preview/StoreImgPreviewThumbs";
+import StoreImageUplForm from "../../../../components/admin_components/stores/forms/StoreImgUplForm";
+import StoreImgPreviewHolder from "../../../../components/admin_components/stores/image_preview/StoreImgPreviewHolder";
+import StoreImgPreviewThumb from "../../../../components/admin_components/stores/image_preview/StoreImgPreviewThumbs";
+import LoadingBar from "../../../../components/admin_components/miscelaneous/LoadingBar";
+// state React.Context //
+import { IGlobalAppState, StateProvider, TestStateProvider } from "../../../../state/Store";
 // helpers //
-import { generateCleanState } from "../../../../test_helpers/miscHelpers";
-import { setMockStoreState } from "../../../../test_helpers/storeHelpers";
-import { IGlobalAppState } from "../../../../state/Store";
+import { createMockStores, setMockStoreState } from "../../../../test_helpers/storeHelpers";
 
 describe("StoreFormHolder Component tests", () => {
   let wrapper: ReactWrapper; 
-  
   describe("Default Form Holder state",  () => {
 
     beforeAll(() => {
       window.scrollTo = jest.fn();
-      wrapper = mount<{}, typeof StoreFormHolder>(<StoreFormHolder state={generateCleanState()} dispatch={jest.fn<React.Dispatch<StoreAction>, []>()}/>)
+      wrapper = mount(
+        <Router>
+          <StoreFormHolder />
+        </Router>
+      );
     });
 
     it("Should Properly Mount Form Holder", () => {
@@ -31,27 +41,35 @@ describe("StoreFormHolder Component tests", () => {
     it("Should have a Form toggle Button", () => {
       const toggleButton = wrapper.find(Button);
       expect(toggleButton.length).toEqual(1);
-    })
-  });
+    });
 
+  });
+  // TEST Form Holder state OPEN - NO Current Store Data //
   describe("Form Holder state OPEN - NO Current Store Data",  () => {
+    let wrapper: ReactWrapper;
 
     beforeAll(() => {
       window.scrollTo = jest.fn();
-      wrapper = mount<{}, typeof StoreFormHolder>(<StoreFormHolder state={generateCleanState()} dispatch={jest.fn<React.Dispatch<StoreAction>, []>()}/>)
-    });
-
-    it("Should Properly Mount Form Holder", () => {
-      const toggleButton = wrapper.find("#storeFormToggleBtn");
-      toggleButton.at(0).simulate("click")
-      // open button clicked //
-      expect(wrapper.html()).toBeDefined();
-
+      wrapper = mount(
+        <Router>
+          <StateProvider>
+            <StoreFormHolder />
+          </StateProvider>
+        </Router>
+      );
     });
     it("Should have a Form toggle Button", () => {
       const toggleButton = wrapper.render().find('#storeFormToggleBtn');
       expect(toggleButton.length).toEqual(1);
     });
+    it("Should Properly Mount Form Holder, respond to '#storeToggleBtn' click", () => {
+      const toggleButton = wrapper.find("#storeFormToggleBtn");
+      toggleButton.at(0).simulate("click")
+      // open button clicked //
+      //wrapper.update()
+      expect(wrapper).toMatchSnapshot();
+    });
+  
     it("Should have a Form Create Button", () => {
       const toggleButton = wrapper.render().find('#adminStoreFormCreate');
       expect(toggleButton.length).toEqual(1);
@@ -68,19 +86,27 @@ describe("StoreFormHolder Component tests", () => {
       const imgUploadForm = wrapper.find(StoreImageUplForm);
       expect(imgUploadForm.length).toEqual(0);
     });
-
   });
-
+  // END Form Holder state OPEN - NO Current Store Data //
+  // TEST Form Holder state OPEN - WITH Current Store Data - NO IMAGES //
   describe("Form Holder state OPEN - WITH Current Store Data - NO IMAGES",  () => {
-    let state: IGlobalAppState;
+    let wrapper: ReactWrapper; let state: IGlobalAppState;
+
     beforeAll(() => {
       window.scrollTo = jest.fn();
       state = setMockStoreState({ currentStore: true });
-      wrapper = mount<{}, typeof StoreFormHolder>(<StoreFormHolder state={state} dispatch={jest.fn<React.Dispatch<StoreAction>, []>()}/>)
+      wrapper = mount(
+        <Router>
+          <TestStateProvider mockState={state}>
+            <StoreFormHolder />
+          </TestStateProvider>
+        </Router>
+      );
+      wrapper.update()
     });
 
     it("Should Properly Mount Form Holder", () => {
-      expect(wrapper.html()).toBeDefined();
+      expect(wrapper.find("#storeFormHolder").length).toEqual(1);
     });
     it("Should have a Form toggle Button", () => {
       const toggleButton = wrapper.render().find('#storeFormToggleBtn');
@@ -106,19 +132,26 @@ describe("StoreFormHolder Component tests", () => {
       const imgUploadForm = wrapper.find(StoreImageUplForm);
       expect(imgUploadForm.length).toEqual(1);
     });
-
   });
-
+  // END Form Holder state OPEN - WITH Current Store Data - NO IMAGES //
+  // TEST Form Holder state OPEN - WITH Current Store Data - WITH IMAGES //
   describe("Form Holder state OPEN - WITH Current Store Data - WITH IMAGES",  () => {
-    let state: IGlobalAppState;
+    let state: IGlobalAppState; let wrapper: ReactWrapper;
+
     beforeAll(() => {
       window.scrollTo = jest.fn();
       state = setMockStoreState({ currentStore: true, storeImages: 3 });
-      wrapper = mount<{}, typeof StoreFormHolder>(<StoreFormHolder state={state} dispatch={jest.fn<React.Dispatch<StoreAction>, []>()}/>)
+      wrapper = mount(
+        <Router>
+          <TestStateProvider mockState={state}>
+            <StoreFormHolder />
+          </TestStateProvider>
+        </Router>
+      );
     });
 
     it("Should Properly Mount Form Holder", () => {
-      expect(wrapper.html()).toBeDefined();
+      expect(wrapper.find("#storeFormHolder").length).toEqual(1);
     });
     it("Should have a Form toggle Button", () => {
       const toggleButton = wrapper.render().find('#storeFormToggleBtn');
@@ -146,5 +179,53 @@ describe("StoreFormHolder Component tests", () => {
       expect(imgUploadForm.length).toEqual(1);
     });
   });
+  // END Form Holder state OPEN - WITH Current Store Data - WITH IMAGES //
+  // TEST Form Holder state OPEN - MOCK Submit action //
+  describe("Form Holder state OPEN - MOCK Submit action",  () => {
+    let state: IGlobalAppState; let wrapper: ReactWrapper;
 
+    beforeAll( async () => {
+      window.scrollTo = jest.fn();
+      // mount and wait //
+      wrapper = mount(
+        <Router initialEntries={["/admin/stores/create"]} >
+          <TestStateProvider>
+            <StoreFormHolder />
+          </TestStateProvider>
+        </Router>
+      );
+    });
+
+    it("Should have a submit button", () => {
+      wrapper.update();
+      wrapper.find("#storeFormToggleBtn").at(0).simulate("click").update();
+      const adminStoreFormCreate = wrapper.find("#adminStoreFormCreate").at(0);
+      expect(adminStoreFormCreate.length).toEqual(1)
+    });
+    it("Should handle the 'handleCreateStoreAction, show 'LoadingBar' Component", async () => {
+      moxios.install();
+      await act( async () => {
+        moxios.stubRequest("/api/stores/create", {
+          status: 200,
+          response: {
+            responseMsg: "All Good",
+            newStore: createMockStores(1)[0]
+          }
+        });
+        const adminStoreFormCreate = wrapper.find("#adminStoreFormCreate").at(0);
+        adminStoreFormCreate.simulate("click");
+        //expect(wrapper.find(LoadingBar).length).toEqual(1);
+      });
+      // expect(sinon.spy(createStore)).toHaveBeenCalled()
+      wrapper.update();
+    });
+    it("Should NOT show the 'LoadingBar' Component after successful API call", () => {
+      expect(wrapper.find(LoadingBar).length).toEqual(0);
+    });
+    it("Should NOT show the 'StoreForm' Component after successful API call", () => {
+      expect(wrapper.find(StoreForm).length).toEqual(0);
+    });
+    // END Form Holder state OPEN - MOCK Submit action //
+  });
+  
 });
