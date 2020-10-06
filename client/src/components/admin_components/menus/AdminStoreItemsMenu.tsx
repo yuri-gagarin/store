c405ac175c4ff4f71a4f4ab4c8f4c7d3778ea6a8
Yuriy Ivanov <ymivanov@icloud.com>
2020-10-06 21:14:18 +0300

    updated AdminStoreItemMenu render tests:

--- client/src/components/admin_components/menus/AdminStoreItemsMenu.tsx
+++ client/src/components/admin_components/menus/AdminStoreItemsMenu.tsx
@@ -1,36 +1,30 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Dropdown, DropdownProps, Menu, MenuItemProps } from "semantic-ui-react";
+import { Menu, MenuItemProps } from "semantic-ui-react";
 // routing //
-import { withRouter, RouteComponentProps, useRouteMatch } from "react-router-dom"
+import { withRouter, RouteComponentProps, useRouteMatch } from "react-router-dom";
+import { AdminStoreItemRoutes } from "../../../routes/adminRoutes";
 // css imports //
 import "./css/adminStoreItemsMenu.css";
-// actions and state //
-import { AppAction, IGlobalAppState } from "../../../state/Store";
-import { getAllStores } from "../stores/actions/APIstoreActions";
-import { getAllStoreItems } from "../store_items/actions/APIStoreItemActions";
 // helpers //
-import { capitalizeString } from "../../helpers/displayHelpers";
 
 interface Props extends RouteComponentProps {
-  state: IGlobalAppState;
   dispatch: React.Dispatch<StoreItemAction>;
 }
 
 const AdminStoreItemsMenu: React.FC<Props> = ({ history, location, dispatch }): JSX.Element => {
   const [ scrolled, setScrolled ] = useState<boolean>(false);
-  const [ activeItem, setActiveItem ] = useState<string>("view_all");
+  const [ activeItem, setActiveItem ] = useState<string>("");
   const [ menuOpen, setMenuOpen ] = useState<boolean>(false);
 
-  const match = useRouteMatch("/admin/home/store_items");
+  const match = useRouteMatch(AdminStoreItemRoutes.HOME_ROUTE);
   const adminStoreItemMenuRef = useRef<HTMLDivElement>(document.createElement("div"));
 
   const handleItemClick = (e: React.MouseEvent, { name }: MenuItemProps): void => {
     setActiveItem(String(name));
 
-
     switch (name) {
       case "view_all": 
-        history.push(match?.path + "/all");
+        history.push(match?.path + "/view_all");
         break;
       case "create":
         history.push(match?.path + "/create");
@@ -39,7 +33,10 @@ const AdminStoreItemsMenu: React.FC<Props> = ({ history, location, dispatch }): 
       case "manage":
         history.push(match?.path + "/manage");
         break;
-      default: history.push("/admin/home");
+      case "view_sorted": 
+        history.push(match?.path + "/view_sorted");
+        break;
+      default: history.push(AdminStoreItemRoutes.HOME_ROUTE);
     }
   }
  
@@ -56,12 +53,14 @@ const AdminStoreItemsMenu: React.FC<Props> = ({ history, location, dispatch }): 
     setTimeout(() => {
       setMenuOpen(true);
     }, 200);
-    if (currentURL.match(/all/)) {
+    if (currentURL.match(/view_all/)) {
       setActiveItem("view_all");
     } else if (currentURL.match(/create/)) {
       setActiveItem("create");
     } else if (currentURL.match(/manage/)) {
       setActiveItem("manage");
+    } else if (currentURL.match(/view_sorted/)) {
+      setActiveItem("view_sorted");
     }
     // menu animation timeout //
     setTimeout(() => {
@@ -87,14 +86,20 @@ const AdminStoreItemsMenu: React.FC<Props> = ({ history, location, dispatch }): 
         />
         <Menu.Item
           name='create'
-          content="Create New StoreItem"
+          content="Create New Store Item"
           active={activeItem === 'create'}
           onClick={handleItemClick}
         />
         <Menu.Item
           name='manage'
-          content="Manage StoreItems"
+          content="Manage Store Items"
           active={activeItem === 'manage'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name="view_sorted"
+          content="Sort Store Items"
+          active={activeItem === "view_sorted"}
           onClick={handleItemClick}
         />
       </Menu>
