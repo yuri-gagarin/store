b55280f7ad85227d7708895dbd93950d7322d637
Yuriy Ivanov <ymivanov@icloud.com>
2020-10-31 22:28:39 +0200

    add render tests for 'delete' Button at 'ProductCard' component

--- client/src/__tests__/components/admin/products/productManageHolder.test.tsx
+++ client/src/__tests__/components/admin/products/productManageHolder.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid } from "semantic-ui-react";
+import { Button, Confirm, Grid } from "semantic-ui-react";
 // test dependencies //
 import moxios from "moxios";
 import { mount, ReactWrapper } from "enzyme";
@@ -325,7 +325,97 @@ describe("Product Manage Holder Tests", () => {
     it(`Should route to a correct client route: ${AdminProductRoutes.MANAGE_ROUTE}`, () => {
       const { history } = wrapper.find(Router).props();
       expect(history.location.pathname).toEqual(AdminProductRoutes.MANAGE_ROUTE);
-    })
+    });
   });
+  // TEST ProductCard DELETE button click //
+  describe("'ProductCard' component DELETE button click action", () => {
+    let wrapper:  ReactWrapper;
+    let mockDeletedProductItem: IProductData;
+    window.scrollTo = jest.fn();
+
+    beforeAll( async () => {
+      const promise = Promise.resolve();
+      moxios.install();
+      moxios.stubRequest("/api/products", {
+        status: 200,
+        response: {
+          responseMsg: "All ok",
+          products: products
+        }
+      });
+      mockDeletedProductItem = products[0];
+      // mount and update //
+      wrapper = mount(
+        <MemoryRouter initialEntries={[ AdminProductRoutes. MANAGE_ROUTE ]} keyLength={0}>
+          <TestStateProvider>
+            <ProductsManageHolder />
+          </TestStateProvider>
+        </MemoryRouter>
+      );
+      await act( async () => promise);
+      wrapper.update();
+    });
+    beforeEach(() => {
+      moxios.install();
+    });
+    afterEach(() => {
+      moxios.uninstall();
+    });
+
+    it("Should render 'Confirm' component after 'DELETE' Button click action", () => {
+      const deleteBtn = wrapper.find(ProductCard).at(0).find(".productCardDeleteBtn");
+      deleteBtn.at(0).simulate("click");
+      const confirmModal = wrapper.find(ProductCard).at(0).find(Confirm);
+      // assert correct rendering //
+      expect(confirmModal.props().open).toEqual(true);
+      expect(confirmModal.find(Button).length).toEqual(2);
+    });
+    it("Should correctly handle {cancelProductDeleteAction} method and update the local component state", () => {
+      const deleteBtn = wrapper.find(ProductCard).at(0).find(".productCardDeleteBtn");
+      deleteBtn.at(0).simulate("click");
+      const confirmModal = wrapper.find(ProductCard).at(0).find(Confirm);
+      // simalute cancel button click //
+      confirmModal.find(Button).at(0).simulate("click");
+      // assert correct rendering //
+      expect(wrapper.find(ProductCard).at(0).find(Confirm).props().open).toEqual(false);
+      expect(wrapper.find(ProductCard).length).toEqual(products.length);
+    });
+    it("Should correctly handle {confirmProductDeleteAction} method and correctly update local component state", async () => {
+      const promise = Promise.resolve();
+      moxios.stubRequest(`/api/products/delete/${products[0]._id}`, {
+        status: 200,
+        response: {
+          responseMsg: "All ok",
+          deletedProduct: mockDeletedProductItem
+        }
+      });
+      // simulate action //
+      const deleteBtn = wrapper.find(ProductCard).at(0).find(".productCardDeleteBtn");
+      deleteBtn.at(0).simulate("click");
+      const confirmModal = wrapper.find(ProductCard).at(0).find(Confirm);
+      // simulate confirm action click //
+      confirmModal.find(Button).at(1).simulate("click");
+      // assert correct 
+      await act( async () => promise);
+      expect(moxios.requests.mostRecent().url).toEqual(`/api/products/delete/${products[0]._id}`);
+    });
+    it("Should correctly updated and rerender 'ProductsManage' component", () => {
+      wrapper.update();
+      expect(wrapper.find(ProductsManageHolder).find(LoadingScreen).length).toEqual(0);
+      expect(wrapper.find(ProductsManageHolder).find(ErrorScreen).length).toEqual(0);
+      expect(wrapper.find(ProductsManageHolder).find(Grid).length).toEqual(1);    
+    });
+    it("Should NOT render the 'removed' 'ProductCard' component", () => {
+      const productCards = wrapper.find(ProductsManageHolder).find(ProductCard);
+      productCards.forEach((productCard) => {
+        expect(productCard.props().product._id).not.toEqual(mockDeletedProductItem._id);
+      });
+    });
+    it("Should rerender with correct number of 'ProductCard' components", () => {
+      const productCards = wrapper.find(ProductsManageHolder).find(ProductCard);
+      expect(productCards.length).toEqual(products.length - 1);
+    });
+
+  })
 
 });
