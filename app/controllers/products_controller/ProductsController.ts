039f89f67a73062060c3ffdec0608327103d86b6
Yuriy Ivanov <ymivanov@icloud.com>
2021-01-02 15:36:37 +0200

    Implement custom middleware for ProductRoutes. Simplified some of the 'ProductsController' methods, all tests and passing

--- app/controllers/products_controller/ProductsController.ts
+++ app/controllers/products_controller/ProductsController.ts
@@ -12,25 +12,30 @@ import { IAdministrator } from "../../models/Administrator";
 import { validateProductData } from "../products_controller/helpers/validationHelpers"; 
 import { NotFoundError, NotAllowedError, processErrorResponse, GeneralError } from "../_helpers/errorHandlers";
 
+/**
+ *  NOTES
+ * By the time any of the 'ProductsController' methods are called some of the 
+ * following is assumed. <Router> should process some custom middleware
+ * including <verifyAdminAndBusinessAccountId> OR <verifyDataModelAccess> OR BOTH!
+ * By the time any 'ProductsController' methods are called, it is assumed that:
+ * 1: <req.user> is defined as 'IAdministrator' model instance
+ * 2: <req.user.businessAccountId> is defined for GET_MANY and CREATE methods
+ * 3: <req.user.businessAccountId> === <product.businessAcccountID> for GET_ONE, EDIT and DELETE methods
+ * 
+ */
+
 class ProductsController implements IGenericController {
 
   getMany (req: Request, res: Response<IGenericProdRes>): Promise<Response> {
     const { name, price, date, limit }: ProducQueryPar = req.query;
+    const { businessAccountId } = req.user as IAdministrator;
     const queryLimit = limit ? parseInt(limit, 10) : 10;
-    // validate an admin present and a valid businessAccountId //
-    const admin = req.user as IAdministrator;
-    if (admin) {
-      if (!admin.businessAccountId) {
-        return respondWithNotAllowedErr(res, "Action not allowed", 401, [ "Create a Business Acccount or request to be added to one to get all features"]);
-      }
-    } else {
-      return respondWithNotAllowedErr(res);
-    }
+   
     // optional queries //
     // sort by price //
     if (price) {
       return (
-        Product.find({})
+        Product.find({ businessAccountId: businessAccountId })
           .sort({ price: price }).limit(queryLimit)
           .populate("images").exec()
       )
@@ -47,7 +52,7 @@ class ProductsController implements IGenericController {
     // sort by date //
     if (date) {
       return (
-        Product.find({})
+        Product.find({ businessAccountId: businessAccountId })
           .sort({ createdAt: date }).limit(queryLimit)
           .populate("images").exec()
       )
@@ -73,8 +78,10 @@ class ProductsController implements IGenericController {
       });
     }
     // general response //
-    return Product.find({ businessAccountId: admin.businessAccountId })
-      .limit(queryLimit).populate("images").exec()
+    return Product.find({ businessAccountId: businessAccountId })
+      .limit(queryLimit)
+      .populate({ path: "images", options: { limit: 1 } })
+      .exec()
       .then((products) => {
         return res.status(200).json({
           responseMsg: `Loaded ${products.length} products`,
@@ -87,40 +94,25 @@ class ProductsController implements IGenericController {
   }
 
   getOne (req: Request, res: Response<IGenericProdRes>): Promise<Response>  {
-    const productId: string = req.params._id;
-    const admin: IAdministrator = req.user as IAdministrator;
-    // assert that an admin sent a product id //
-    if (!productId) {
-      return respondWithInputError(res, "Input error", 404, [ "Can't resolve product"]);
-    }
-    // assert that an admin exists and has a Business Account set up //
-    if (admin) {
-      if (!admin.businessAccountId) {
-        return respondWithNotAllowedErr(res, "Action not allowed", 401, [ "Create a Business Acccount or request to be added to one to get all features"]);
-      }
-    } else {
-      return respondWithNotAllowedErr(res);
-    }
-    //
-    return Product.findOne({ _id: productId })
-      .then((product) => {
-        if (product) {
-          if ((product.businessAccountId as Types.ObjectId).equals(admin.businessAccountId!)) {
-            return (
-              product.populate("images").execPopulate()
-            );
-          } else {
-            throw new NotAllowedError({ messages: [ "Your account canot access other accounts Products" ] });
-          }
-        } else {
-          throw new NotFoundError({ messages: [ "Product was not found" ] });
-        }
-      })
-      .then((product) => {
+    const { businessAccountId } = req.user as IAdministrator;
+    const { productId } = req.params;
+
+    return (
+      Product.findOne({ businessAccountId: businessAccountId, _id: productId })
+        .populate({ path: "images" })
+        .exec()
+    )
+    .then((product) => {
+      if (product) {  
         return res.status(200).json({
           responseMsg: "Returned product",
           product: product
         });
+      } else {
+        throw new NotFoundError({
+          messages: [ "Could not find the queried Product" ]
+        })
+      }
       })
       .catch((error) => {
         return processErrorResponse(res, error);
@@ -129,15 +121,9 @@ class ProductsController implements IGenericController {
 
   create (req: Request, res: Response<IGenericProdRes>): Promise<Response> {
     const { name, description, details, price }: ProductData = req.body;    
-    const admin: IAdministrator = req.user as IAdministrator;
+    const { businessAccountId }: IAdministrator = req.user as IAdministrator;
     // assert that a user is present //
-    if(!admin) {
-      return respondWithGeneralError(res, "Cannot resolve user", 401);
-    }
-    // assert that an admin has a BusinessAccount set up //
-    if (!admin.businessAccountId) {
-      return respondWithInputError(res, "Account error", 401, [ "You must have or be tied to an account to create a Product "]);
-    }
+    
     // validate form input //
     const { valid, errorMessages } = validateProductData({ name, price, description, details });
     if(!valid) {
@@ -145,7 +131,7 @@ class ProductsController implements IGenericController {
     }
 
     const newProduct = new Product({
-      businessAccountId: admin.businessAccountId,
+      businessAccountId: businessAccountId,
       name: name,
       description: description,
       details: details,
@@ -168,24 +154,13 @@ class ProductsController implements IGenericController {
   }
 
   edit (req: Request, res: Response<IGenericProdRes>): Promise<Response> {
-    const { _id : productId } = req.params;
+    const { productId } = req.params;
+    const { businessAccountId } = req.user as IAdministrator;
     const { name, description, details, price, images : productImages = [] }: ProductData = req.body;
-    const updatedProductImgs: Types.ObjectId[] = [];
+    const updatedProductImgs: string[] = [];
 
     const admin: IAdministrator = req.user as IAdministrator;
-    // ensure a productId was sent //
-    if (!productId) {
-      return respondWithInputError(res, "Can't resolve Product", 400);
-    }
-    // ensure that a user has an account set up and that a user can edit this product //
-    if(admin) {
-      if(!admin.businessAccountId) {
-        return respondWithInputError(res, "Account error", 401, [ "You must have or be tied to an account to update a Product" ]);
-      }
-    } else {
-      return respondWithGeneralError(res, "Cannot resolve user", 401);
-    }
-    //
+    
     // validate correct data //
     const { valid, errorMessages } = validateProductData({ name, price, description, details });
     if (!valid) {
@@ -193,115 +168,106 @@ class ProductsController implements IGenericController {
     }
     //
 
-    return Product.findOne({ _id: productId })
-      .then((product) => {
-        
-        if (product) {
-          if (String(product.businessAccountId) === String(admin.businessAccountId)) {
-            return Product.findOneAndUpdate(
-              { _id: product._id },
-              { $set: { 
-                  name: name, 
-                  price: (price as number),
-                  description: description, 
-                  details: details, 
-                  editedAt: new Date(Date.now()), 
-                  images: [ ...(productImages as Types.ObjectId[]) ] 
-                } 
-              },
-              { new: true }
-            )
-            .populate("images").exec();
-          } else {
-            throw new NotAllowedError({ messages: [ "Cannot update a Product which doesnt belong to your account" ] });
+    return (
+      Product.findOneAndUpdate(
+        { businessAccountId: businessAccountId, _id: productId },
+        { $set: { 
+            name: name, 
+            price: (price as number),
+            description: description, 
+            details: details, 
+            editedAt: new Date(Date.now()), 
+            images: [ ...(productImages as Types.ObjectId[]) ] 
           }
-        } else {
-          throw new NotFoundError({ messages: [ "Cannot resolve Product to update" ] });
-        }
-      })
-      .then((editedProduct) => {
+        },
+        { new: true }
+      )
+      .populate("images")
+      .exec()
+    )
+    .then((updatedProduct) => {
+      if (updatedProduct) {
         return res.status(200).json({
-          responseMsg: "Product updated",
-          editedProduct: editedProduct!
+          responseMsg: "Product successfuly updated",
+          editedProduct: updatedProduct
         });
-      })
-      .catch((error) => {
-        return processErrorResponse(res, error);
-      });
+      } else {
+        throw new NotFoundError({
+          errMessage: "Product update error",
+          messages: [ "Could not resolved the queried Product to update" ]
+        });
+      }
+    })
+    .catch((error) => {
+      return processErrorResponse(res, error);
+    });
   }
 
   delete (req: Request, res: Response<IGenericProdRes>): Promise<Response> {
-    let deletedImages: number;
+    const { businessAccountId } = req.user as IAdministrator;
+    const { productId } = req.params;
     let productImage: IProductImage;
-    const productImagePaths: string[] = [];
-    const productImageIds: string[] = [];
-    const deletePromises: Promise<boolean>[] = [];
-
-    const { _id: productId } = req.params;
-    
-    const admin: IAdministrator = req.user as IAdministrator;
+    let numOfImagesDeleted: number = 0;
     // ensure that an admin is present with a business account //
-    if (admin) {
-      if (!admin.businessAccountId) {
-        return respondWithNotAllowedErr(res, "Not Allowed", 401, [ "Not allowed to delete Products without a Business Account set up " ]);
+  
+    return (
+      Product.findOne({ businessAccountId: businessAccountId, _id: productId })
+        .populate({ path: "images", options: { limit: 1 } })
+        .exec()
+    )
+    .then((product) => {
+      // first assert that a correct admin is deleting //
+      if (product) {
+        return Promise.resolve(product);
+      } else {
+        throw new NotFoundError({ messages: [ "Can't resolve Product to delete" ] });
       }
-    } else {
-      return respondWithGeneralError(res, "Cannot resolve Admin", 401);
-    }
-    // ensure that a product id was sent /
-    if (!productId) {
-      return respondWithInputError(res, "Can't resolve Product");
-    }
-
-    return Product.findOne({ _id: productId })
-      .populate({ path: "images", options: { limit: 1 } })
-      .exec()
-      .then((product) => {
-        // first assert that a correct admin is deleting //
-        if (product) {
-          return Promise.resolve(product);
-        } else {
-          throw new NotFoundError({ messages: [ "Can't resolve Product to delete" ] });
-        }
-      })
-      .then((product) => {
-        if (String(product.businessAccountId) === String(admin.businessAccountId)) {
-          // assuming the right account is deleting first resolve images //
-          if (product.images.length > 0) productImage = (product.images[0] as IProductImage);
-          return Promise.resolve();
-        } else {
-          throw new NotAllowedError({ messages: [ "Not allowed to delete a Product which does not belong to your Account" ] });
-        }
-      })
-      .then((_) => {
-        if (productImage && productImage.absolutePath) {
-          const imagesDirectory = resolveDirectoryOfImg(productImage.absolutePath);
-          return removeDirectoryWithFiles(imagesDirectory);
-        } else {
-          return Promise.resolve({ success: true, numberRemoved: 0, message: "No images" });
-        }
-      })
-      .then((_) => {
+    })
+    .then((product) => {
+      if (product.images.length > 0) { 
+          productImage = (product.images[0] as IProductImage);
+        return removeDirectoryWithFiles(productImage.imagePath);
+      } else {
+        return Promise.resolve({ success: true, numberRemoved: 0 });
+      }
+    })
+    .then((_) => {
+      if (productImage && productImage.absolutePath) {
+        const imagesDirectory = resolveDirectoryOfImg(productImage.absolutePath);
+        return removeDirectoryWithFiles(imagesDirectory);
+      } else {
+        return Promise.resolve({ success: true, numberRemoved: 0, message: "No images" });
+      }
+    })
+    .then(({ numberRemoved }) => {
+      if (numberRemoved > 0) {
         return ProductImage.deleteMany({ productId: productId }).exec();
-      })
-      .then(({ n }) => {
-        n ? deletedImages = n : 0;
-        return Product.findOneAndDelete({ _id: productId }).exec();
-      })  
-      .then((product) => {
-        if (product) {
-          return res.status(200).json({
-            responseMsg: "Deleted the Product and " + deletedImages,
-            deletedProduct: product
-          });
-        } else {
-          throw new GeneralError("Something went very wrong", 500);
-        }
-      })
-      .catch((err) => {
-        return processErrorResponse(res, err);
-      });
-    
+      } else {
+        return Promise.resolve({ response: { ok: true, n: 0 }, deletedCount: 0 })
+      }
+    })
+    .then(({ ok, deletedCount }) => {
+      if (ok && deletedCount && (deletedCount > 0)) numOfImagesDeleted = deletedCount;
+      return (
+        Product.findOneAndDelete({ 
+          businessAccountId: businessAccountId, _id: productId 
+        })
+        .exec()
+      );
+    })  
+    .then((product) => {
+      if (product) {
+        return res.status(200).json({
+          responseMsg: "Deleted the Product and " + numOfImagesDeleted,
+          deletedProduct: product
+        });
+      } else {
+        throw new GeneralError("Something went very wrong", 500);
+      }
+    })
+    .catch((err) => {
+      return processErrorResponse(res, err);
+    });
   }
 }
 
--- app/routes/ProductRoutes.ts
+++ app/routes/ProductRoutes.ts
@@ -2,13 +2,15 @@ import { Router} from "express";
 import passport from "passport";
 import { RouteConstructor } from "./helpers/routeInterfaces";
 import { IGenericController } from "../controllers/_helpers/controllerInterfaces";
+// custom middleware //
+import { verifyAdminAndBusinessAccountId, verifyDataModelAccess } from "../custom_middleware/customMiddlewares";
 
 class ProductRoutes extends RouteConstructor<IGenericController> {
   private viewAllProductsRoute = "/api/products";
-  private viewProductRoute = "/api/products/:_id";
+  private viewProductRoute = "/api/products/:productId";
   private createProductRoute = "/api/products/create";
-  private editProductRoute = "/api/products/update/:_id";
-  private deleteProductRoute = "/api/products/delete/:_id";
+  private editProductRoute = "/api/products/update/:productId";
+  private deleteProductRoute = "/api/products/delete/:productId";
   
   constructor (router: Router, controller: IGenericController) {
     super(router, controller);
@@ -24,27 +26,60 @@ class ProductRoutes extends RouteConstructor<IGenericController> {
   private getAllProducts (): void {
     this.Router
       .route(this.viewAllProductsRoute)
-      .get(passport.authenticate("adminJWT", { session: false }), this.controller.getMany);
+      .get(
+        [
+          passport.authenticate("adminJWT", { session: false }),    // passport middleware jwt token authentication //
+          verifyAdminAndBusinessAccountId                           // custom middleware to verify the presence of <req.user> and <req.user.businessAccountId> //
+        ],
+        this.controller.getMany
+      );
   }
   private getProduct (): void {
     this.Router
       .route(this.viewProductRoute)
-      .get(passport.authenticate("adminJWT", { session: false }), this.controller.getOne);
+      .get(
+        [
+          passport.authenticate("adminJWT", { session: false }),    // passport middleware jwt token authentication //
+          verifyAdminAndBusinessAccountId,                          // custom middleware to verify the presence of <req.user> and <req.user.businessAccountId> //
+          verifyDataModelAccess                                     // custom middleware to ensure <req.user.businessAccountId> === <product.businessAccountId> //
+        ],
+        this.controller.getOne
+      );
   }
   private createProduct (): void {
     this.Router
       .route(this.createProductRoute)
-      .post(passport.authenticate("adminJWT", { session: false }), this.controller.create);
+      .post(
+        [
+          passport.authenticate("adminJWT", { session: false }),    // passport middleware jwt token authentication //
+          verifyAdminAndBusinessAccountId,                          // custom middleware to verify the presence of <req.user> and <req.user.businessAccountId> //
+        ],
+        this.controller.create
+      );
   }
   private editProduct (): void {
     this.Router
       .route(this.editProductRoute)
-      .patch(passport.authenticate("adminJWT", { session: false }), this.controller.edit);
+      .patch(
+        [
+          passport.authenticate("adminJWT", { session: false }),   // passport middleware jwt token authentication //
+          verifyAdminAndBusinessAccountId,                         // custom middleware to verify the presence of <req.user> and <req.user.businessAccountId> //
+          verifyDataModelAccess                                    // custom middleware to ensure <req.user.businessAccountId> === <product.businessAccountId> //
+        ],
+        this.controller.edit)
+      ;
   }
   private deleteProduct (): void {
     this.Router
       .route(this.deleteProductRoute)
-      .delete(passport.authenticate("adminJWT", { session: false }), this.controller.delete);
+      .delete(
+        [ 
+          passport.authenticate("adminJWT", { session: false }),   // passport middleware jwt token authentication //
+          verifyAdminAndBusinessAccountId,                         // custom middleware to verify the presence of <req.user> and <req.user.businessAccountId> // 
+          verifyDataModelAccess                                    // custom middleware to ensure <req.user.businessAccountId> === <product.businessAccountId> //
+        ],
+        this.controller.delete
+      );
   }
 }
 
