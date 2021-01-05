8a73e7602f40827acbc09d07a8d4cfed72baadf5
Yuriy Ivanov <ymivanov@icloud.com>
2021-01-05 17:25:32 +0200

    Implementation of  'ServicesController - Logged In WITH CORRECT BusinessAccount ID - tests with 'ServiceImage' models present - GET/POST/PATCH/DELETE  - API tests' test suite, more detailed cases to be added

--- app/_tests_/api/services_controller/helpers/setupServiceControllerTests.ts
+++ app/_tests_/api/services_controller/helpers/setupServiceControllerTests.ts
@@ -83,3 +83,4 @@ export const setupServiceControllerTests = (): Promise<SetupServiceContTestRes> 
       throw err;
     });
 };
+
--- app/_tests_/api/services_controller/withImagesTest.test.ts
+++ app/_tests_/api/services_controller/withImagesTest.test.ts
@@ -2,20 +2,24 @@
 import chai, { expect } from "chai";
 import chaiHTTP from "chai-http";
 import { Types } from "mongoose";
+import fs, { readdirSync } from "fs";
+import path from "path";
 // server import /
 import server from "../../../server";
 // models and model interfaces //
 import BusinessAccount from "../../../models/BusinessAccount";
 import { ServiceData } from "../../../controllers/services_controller/type_declarations/servicesControllerTypes";
+import ServiceImage, { IServiceImage } from "../../../models/ServiceImage";
 import { IAdministrator } from "../../../models/Administrator";
 import Service, { IService } from "../../../models/Service";
 // helpers //
 import { clearDB } from "../../helpers/dbHelpers";
 import { generateMockServiceData } from "../../helpers/data_generation/serviceDataGeneration"; 
 import { setupServiceControllerTests } from "./helpers/setupServiceControllerTests";
+import { cleanUpServiceImgControllerTests } from "../service_images_controller/helpers/setupServiceImgControllerTests";
 import { createServiceImages } from "../../helpers/data_generation/serviceImageDataGeneration";
 import { loginAdmins } from "../../helpers/auth_helpers/authHelpers";
-import ServiceImage, { IServiceImage } from "../../../models/ServiceImage";
+import { image } from "faker";
 
 chai.use(chaiHTTP);
 /**
@@ -26,6 +30,7 @@ chai.use(chaiHTTP);
 
 describe("ServicesController - Logged In WITH CORRECT BusinessAccount ID - tests with 'ServiceImage' models present - GET/POST/PATCH/DELETE  - API tests", () => {
   let firstAdmin: IAdministrator, secondAdmin: IAdministrator, thirdAdmin: IAdministrator;
+  let firstAdminBusAcctId: string, secondAdminBusAcctId: string;
   // service models //
   let service: IService;
   let fetchedServices: IService[];
@@ -42,6 +47,10 @@ describe("ServicesController - Logged In WITH CORRECT BusinessAccount ID - tests
   // mockData //
   let newServiceData: ServiceData;
   let updateServiceData: ServiceData;
+  // 
+  let numberOfServiceImageDirectories: number = 0;    // <path>/public/uploads/service_images/<businessAccountId> //
+  let numberOfServiceImageSubDirectories: number = 0; // <path>/public/uploads/service_images/<businessAccountId>/<serviceId> //
+  let numberOfServiceImageFiles: number = 0;
 
   // database and model data setup //
   // logins and jwtTokens //
@@ -50,6 +59,7 @@ describe("ServicesController - Logged In WITH CORRECT BusinessAccount ID - tests
       .then((response) => {
         ({ firstAdmin, secondAdmin } = response.admins);
         ({ firstAdminsService, secondAdminsService } = response.services);
+        ({ firstAdminBusAcctId, secondAdminBusAcctId } = response.busAccountIds);
         return loginAdmins(chai, server, [ firstAdmin, secondAdmin ]);
       })
       .then((tokensArr) => {
@@ -69,7 +79,27 @@ describe("ServicesController - Logged In WITH CORRECT BusinessAccount ID - tests
       })
       .then((countsArray) => {
         [ totalServices, totalServiceImages, firstAdminsServiceImgTotal ] = countsArray;
-        done();
+        try {
+          const directories = fs.readdirSync(path.join(path.resolve(), "public", "uploads", "service_images"));
+          const subDirectories: string[] = [];
+          const createdImageFiles: string[] = [];
+          for (const directory of directories) {
+            const subdirs: string[] = fs.readdirSync(path.join(path.resolve(), 'public', 'uploads', 'service_images', directory));
+            for (const subdir of subdirs) {
+              subDirectories.push(path.join(path.resolve(), "public", "uploads", "service_images", directory, subdir));
+            }
+          }
+          for (const subDir of subDirectories) {
+            const files = fs.readdirSync(subDir);
+            createdImageFiles.push(...files);
+          }
+          numberOfServiceImageDirectories = directories.length;
+          numberOfServiceImageSubDirectories = subDirectories.length;
+          numberOfServiceImageFiles = createdImageFiles.length;
+          done();
+        } catch (error) {
+          done(error);
+        }
       })
       .catch((err) => {
         done(err);
@@ -79,10 +109,20 @@ describe("ServicesController - Logged In WITH CORRECT BusinessAccount ID - tests
     [ newServiceData, updateServiceData ] = generateMockServiceData(2);
   });
   after((done) => {
-    clearDB().then(() => done()).catch((err) => done(err));
+    Promise.all([
+      clearDB(),
+      cleanUpServiceImgControllerTests(firstAdminBusAcctId, secondAdminBusAcctId)
+    ])
+    .then(() => {
+      done();
+    })
+    .catch((error) => {
+      done(error);
+    });
   });
 
   context("Admin WITH a 'BusinessAccount' set up, GET_MANY, GET_ONE, CREATE, EDIT, DELETE actions", () => {
+
     // TEST GET GET_MANY action correct BusinessAccount //
     describe("GET '/api/services' - CORRECT 'BusinessAccount' tests with 'ServiceImages' - GET_MANY action", () => {
 
@@ -133,13 +173,13 @@ describe("ServicesController - Logged In WITH CORRECT BusinessAccount ID - tests
 
     });
     // END TEST GET GET_MANY action correct BusinessAccount //
-    
-    // TEST GET GET_ONE action correct BusinessAccount //
-    describe("GET '/api/services/:serviceId' - CORRECT 'BusinessAccount' tests with 'ServiceImages' - GET_ONE action", () => {
+
+    // TEST GET_ONE action with correct BusinessAccount //
+    describe("GET '/api/services/:serviceId' - CORRECT 'BusinessAccount' tests with 'ServiceImages' - GET_MANY action", () => {
 
       it("Should fetch 'Service' models and return a correct response", (done) => {
         chai.request(server)
-          .get("/api/services/" + String(firstAdminsService._id))
+          .get("/api/services/" + (firstAdminsService._id as Types.ObjectId))
           .set({ "Authorization": firstToken })
           .end((err, res) => {
             if (err) done(err);
@@ -153,20 +193,19 @@ describe("ServicesController - Logged In WITH CORRECT BusinessAccount ID - tests
             done();
           });
       });
-      it("Should return the correct 'Service' model and correctly populate the 'ServiceImage' model subarray", () => {
-        expect(service.images.length).to.equal(firstAdminsServiceImgTotal);
-        for (const serviceImg of service.images as IServiceImage[]) {
-          expect(serviceImg.businessAccountId).to.be.an("string");
-          expect(serviceImg.serviceId).to.be.an("string");
-          expect(serviceImg.imagePath).to.be.a("string");
-          expect(serviceImg.absolutePath).to.be.a("string");
-          expect(serviceImg.url).to.be.a("string");
-          expect(serviceImg.fileName).to.be.a("string");
-          expect(serviceImg.createdAt).to.be.an("string");
-          expect(serviceImg.editedAt).to.be.an("string");
+      it("Should correctly populate the 'Service' model's <images> subarray with the 'ServiceImage' models", () =>{
+        for (const serviceImage of service.images as IServiceImage[]) {
+          expect(serviceImage.businessAccountId).to.be.an("string");
+          expect(serviceImage.serviceId).to.be.an("string");
+          expect(serviceImage.imagePath).to.be.a("string");
+          expect(serviceImage.absolutePath).to.be.a("string");
+          expect(serviceImage.url).to.be.a("string");
+          expect(serviceImage.fileName).to.be.a("string");
+          expect(serviceImage.createdAt).to.be.an("string");
+          expect(serviceImage.editedAt).to.be.an("string");
         }
       });
-      it("Should NOT alter any 'ServiceImage' models in the database", (done) => {
+      it("Should NOT alter anything within 'ServiceImage' models in the database", (done) => {
         ServiceImage.countDocuments().exec()
           .then((number) => {
             expect(number).to.equal(totalServiceImages);
@@ -176,20 +215,157 @@ describe("ServicesController - Logged In WITH CORRECT BusinessAccount ID - tests
             done(error);
           });
       });
-      it("Should NOT alter the number of 'ServiceImage' models in the queried 'Service' model", (done) => {
-        Service.findOne({ _id: firstAdminsService._id }).exec()
-          .then((service) => {
-            expect(service!.images.length).to.equal(firstAdminsServiceImgTotal);
+      it("Should NOT remove any 'ServiceImage' files belonging to queried 'Service' model", () => {
+        const imgDirectory = firstAdminsServiceImages[0].imagePath;
+        try {
+          const imageFiles = fs.readdirSync(imgDirectory);
+          expect(imageFiles.length).to.equal(firstAdminsServiceImgTotal);
+        } catch (error) {
+          throw error;
+        }
+      });
+
+    });
+    // END TEST GET_ONE action with a correct BusinessAccount //
+
+    // TEST POST CREATE action with a correct BusinessAccount //
+    describe("POST '/api/services/create' - CORRECT 'BusinessAccount' tests with 'ServiceImages' - CREATE action", () => {
+
+      it("Should create a 'Service' model and return a correct response", (done) => {
+        const mockService = generateMockServiceData(1);
+        chai.request(server)
+          .post("/api/services/create")
+          .set({ "Authorization": firstToken })
+          .send( ...mockService )
+          .end((err, res) => {
+            if (err) done(err);
+            createdService = res.body.newService;
+            // assert correct response //
+            expect(res.status).of.equal(200);
+            expect(res.body.responseMsg).be.a("string");
+            expect(res.body.newService).to.be.an("object");
+            expect(res.body.error).to.be.undefined;
+            expect(res.body.errorMessages).to.be.undefined;
+            done();
+          });
+      });
+      it("Should NOT alter anything within 'ServiceImage' models in the database", (done) => {
+        ServiceImage.countDocuments().exec()
+          .then((number) => {
+            expect(number).to.equal(totalServiceImages);
             done();
           })
           .catch((error) => {
             done(error);
-          })
+          });
       });
+      it("Should NOT create any new 'ServiceImage; directories nor alter eny existing directories", () => {
+        try {
+          const imgDirectories = fs.readdirSync(path.join(path.resolve(), "public", "uploads", "service_images"));
+          const absoluteImgDirectories: string[] = [];
+          const imageFiles: string[] = [];
+          for (const directory of imgDirectories) {
+            const subdirs = fs.readdirSync(path.join(path.resolve(), "public", "uploads", "service_images", directory));
+            for (const subdir of subdirs) {
+              absoluteImgDirectories.push(path.join(path.resolve(), "public", "uploads", "service_images", directory, subdir));
+            }
+          }
+          for (const absDirectory of absoluteImgDirectories) {
+            const files = fs.readdirSync(absDirectory);
+            imageFiles.push(...files);
+          }
+          expect(imgDirectories.length).to.equal(numberOfServiceImageDirectories);
+          expect(absoluteImgDirectories.length).to.equal(numberOfServiceImageSubDirectories);
+          expect(imageFiles.length).to.equal(numberOfServiceImageFiles);
+        } catch (error) {
+          throw error;
+        }
+      });
+
     });
-    // END TEST GET_ONE action correct BusinessAccount //
+
+    // END TEST POST CREATE action with a corect BusinessAccount //
+
+    // TEST DELETE DELETE action correct BusinessAccount //
+    describe("DELETE '/api/services/delete/:serviceId' - CORRECT 'BusinessAccount' tests with 'ServiceImages' - DELETE action", () => {
+
+      it("Should deleted the  'Service' model and return a correct response", (done) => {
+        chai.request(server)
+          .delete("/api/services/delete/" + String(firstAdminsService._id))
+          .set({ "Authorization": firstToken })
+          .end((err, res) => {
+            if (err) done(err);
+            service = res.body.service;
+            // assert correct response //
+            expect(res.status).of.equal(200);
+            expect(res.body.responseMsg).be.a("string");
+            expect(res.body.deletedService).to.be.an("object");
+            expect(res.body.error).to.be.undefined;
+            expect(res.body.errorMessages).to.be.undefined;
+            done();
+          });
+      });
+      it("Should remove correct 'ServiceImage' models in the database", (done) => {
+        ServiceImage.find({ serviceId: firstAdminsService._id }).exec()
+          .then((services) => {
+            expect(services.length).to.equal(0);
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should decrement the 'ServiceImage' models in the database by a correct number", (done) => {
+        ServiceImage.countDocuments().exec() 
+          .then((count) => {
+            expect(count).to.equal(totalServiceImages - firstAdminsServiceImgTotal);
+            totalServiceImages -= firstAdminsServiceImgTotal;
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should remove the 'ServiceImage' directories for the queried 'ServiceItem'", () => {
+        const serviceImgDirectory = firstAdminsServiceImages[0].imagePath;
+        try {
+          fs.accessSync(serviceImgDirectory);
+        } catch(error) {
+          expect(error.code).to.equal("ENOENT");
+        }
+      });
+      it("Should correctly handle image file removal, remove appropriate files and directories only", () => {
+        // service images are uploaded to <path>/public/uploads/service_images/<businessAccounId>/<serviceId> //
+        // a removed service should remove its linked uploaded images in <path>/public/uploads/service_images/<businessAccounId>/<serviceId> directory //
+        // it should also remove the <serviceId> directory but leave all other directories untouched //
+        const imgDirectories = fs.readdirSync(path.join(path.resolve(), "public", "uploads", "service_images"));
+        const absoluteImgSubdirectories: string[] = [];
+        const imageFiles: string[] = [];
+        try {
+          for (const directory of imgDirectories) {
+            const subdirs = fs.readdirSync(path.join(path.resolve(), "public", "uploads", "service_images", directory));
+            for (const subdir of subdirs) {
+              absoluteImgSubdirectories.push(path.join(path.resolve(), "public", "uploads", "service_images", directory, subdir));
+            }
+          }
+          for (const absoluteImgSubDirectory of absoluteImgSubdirectories) {
+            const imgFiles = readdirSync(absoluteImgSubDirectory);
+            imageFiles.push(...imgFiles);
+          }
+          // assert correct reponse //
+          // image directory should not be deleted, only the subdirectory belonging to queried account model //
+          expect(imgDirectories.length).to.equal(numberOfServiceImageDirectories);
+          expect(absoluteImgSubdirectories.length).to.equal(numberOfServiceImageSubDirectories - 1);
+          expect(imageFiles.length).to.equal(numberOfServiceImageFiles - firstAdminsServiceImgTotal);
+        } catch (error) {
+          throw error;
+        }
+      })
+    });
+    // END TEST DELETE DELETE action correct BusinessAccount //
+
    
-  
+    
   });
  
 });
