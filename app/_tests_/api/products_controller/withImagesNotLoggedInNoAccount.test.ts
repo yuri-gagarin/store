be295ab4d7d868aa0e63262cc77cffbc9b5220a1
Yuriy Ivanov <ymivanov@icloud.com>
2021-01-07 15:34:42 +0200

    Full implementation of 'ProductsController - NOT LOGGED IN or NO BUSINESS ACCOUNT - tests with 'ProductImage' models present - GET/POST/PATCH/DELETE  - API tests', all test cases are pasing

--- app/_tests_/api/products_controller/withImagesNotLoggedInNoAccount.test.ts
+++ app/_tests_/api/products_controller/withImagesNotLoggedInNoAccount.test.ts
@@ -12,9 +12,10 @@ import Product, { IProduct } from "../../../models/Product";
 // helpers //
 import { clearDB } from "../../helpers/dbHelpers";
 import { generateMockProductData } from "../../helpers/data_generation/productsDataGeneration"; 
-import { setupProdControllerTests } from "./helpers/setupProdControllerTest";
+import { loginAdmins, setupProdControllerTests } from "./helpers/setupProdControllerTest";
 import { cleanUpProductImgControllerTests } from "../product_images_controller/helpers/setupProdImgControllerTest";
 import { createProductImages } from "../../helpers/data_generation/productImageDataGeneration";
+import { IAdministrator } from "../../../models/Administrator";
 
 chai.use(chaiHTTP);
 
@@ -74,6 +75,8 @@ const getImageUploadData = (...paths: string []): ImageDirectoryDetails => {
 
 describe("ProductsController - NOT LOGGED IN or NO BUSINESS ACCOUNT - tests with 'ProductImage' models present - GET/POST/PATCH/DELETE  - API tests", () => {
   let firstAdminBusAcctId: string, secondAdminBusAcctId: string;
+  let thirdAdmin: IAdministrator;
+  let thirdAdminsToken: string;
   // product models //
   let firstAdminsProduct: IProduct;
   let firstAdminsProductsArr: IProduct[], secondAdminsProductsArr: IProduct[];
@@ -91,6 +94,7 @@ describe("ProductsController - NOT LOGGED IN or NO BUSINESS ACCOUNT - tests with
     setupProdControllerTests()
       .then((response) => {
         ({ firstAdminBusAcctId, secondAdminBusAcctId } = response.busAccountIds);
+        ({ thirdAdmin } = response.admins );
         ({ firstAdminsProductsArr, secondAdminsProductsArr, firstAdminsProduct } = response.products);
         const productImgPromises: Promise<IProductImage[]>[] = [];
         for (const product of firstAdminsProductsArr) {
@@ -103,9 +107,12 @@ describe("ProductsController - NOT LOGGED IN or NO BUSINESS ACCOUNT - tests with
         }
         return Promise.all(productImgPromises);
       })
-      .then((productImages) => {
+      .then((_) => {
+        return loginAdmins(chai, server, [ thirdAdmin ]);
+      })
+      .then((adminsTokenArray) => {
+        [ thirdAdminsToken ] = adminsTokenArray;
         return Promise.all([
-          Product.countDocuments().exec(),
           ProductImage.countDocuments().exec(),
           ProductImage.countDocuments({ productId: firstAdminsProduct._id }).exec()
         ]);
@@ -138,7 +145,7 @@ describe("ProductsController - NOT LOGGED IN or NO BUSINESS ACCOUNT - tests with
       done(error);
     });
   });
-
+  // TEST CONTEXT Admin is not logged in 'ProductsController' actions tests //
   context("Admin NOT LOGGED IN, GET_MANY, GET_ONE, CREATE, EDIT, DELETE actions", () => {
 
     // TEST GET GET_MANY action  not logged in //
@@ -182,7 +189,7 @@ describe("ProductsController - NOT LOGGED IN or NO BUSINESS ACCOUNT - tests with
     // END TEST GET GET_MANY action not logged in //
 
     // TEST GET_ONE action not logged in //
-    describe("GET '/api/products/:productId' - NOT LOGGED IN tests with 'ProductImages' - GET_MANY action", () => {
+    describe("GET '/api/products/:productId' - NOT LOGGED IN - tests with 'ProductImages' - GET_MANY action", () => {
     
       it("Should NOT fetch 'Product' models and return a correct error response", (done) => {
         chai.request(server)
@@ -234,7 +241,7 @@ describe("ProductsController - NOT LOGGED IN or NO BUSINESS ACCOUNT - tests with
     // END TEST GET_ONE action not logged in //
 
     // TEST POST CREATE not logged in //
-    describe("POST '/api/products/create' - NOT LOGGED IN tests with 'ProductImages' - CREATE action", () => {
+    describe("POST '/api/products/create' - NOT LOGGED IN - tests with 'ProductImages' - CREATE action", () => {
 
       it("Should NOT create a 'Product' model and return a correct error response", (done) => {
         const mockProduct = generateMockProductData(1);
@@ -278,7 +285,7 @@ describe("ProductsController - NOT LOGGED IN or NO BUSINESS ACCOUNT - tests with
     // END TEST POST CREATE action not logged in //
 
     // TEST PATCH EDIT  action not logged in //
-    describe("POST '/api/products/update/:productId' - NOT LOGGED IN - tests with 'ProductImages' - CREATE action", () => {
+    describe("PATCH '/api/products/update/:productId' - NOT LOGGED IN - tests with 'ProductImages' - EDIT action", () => {
 
       it("Should NOT update 'Product' model and return a correct response", (done) => {
         const mockProduct = generateMockProductData(1);
@@ -384,5 +391,254 @@ describe("ProductsController - NOT LOGGED IN or NO BUSINESS ACCOUNT - tests with
     // END TEST DELETE DELETE action correct BusinessAccount //
     
   });
-  // END CONTEXT tests with a correct business accound id //
+  // END TEST CONTEXT Admin is not logged in 'ProductsController' actions tests //
+  
+  // TEST CONTEXT Admin is logged in 'ProductsController' actions tests no business account //
+  context("Admin LOGGED IN no BUSINESS ACCOUNT, GET_MANY, GET_ONE, CREATE, EDIT, DELETE actions", () => {
+
+    // TEST GET GET_MANY action  logged in no account //
+    describe("GET '/api/products' - LOGGED IN no ACCOUNT' - tests with 'ProductImages' - GET_MANY action", () => {
+      it("Should NOT fetch 'Product' models and return a correct error response", (done) => {
+        chai.request(server)
+          .get("/api/products")
+          .set({ "Authorization": thirdAdminsToken })
+          .end((err, res) => {
+            if (err) done(err);
+            // assert correct response //
+            expect(res.status).of.equal(401);
+            expect(res.body.responseMsg).be.a("string");
+            expect(res.body.error).to.be.an("object");
+            expect(res.body.errorMessages).to.be.an("array");
+            expect(res.body.products).to.be.undefined;
+            done();
+          });
+      });
+      it("Should NOT alter anything within 'ProductImage' models in the database", (done) => {
+        ProductImage.countDocuments().exec()
+          .then((number) => {
+            expect(number).to.equal(totalProductImages);
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should NOT alter any 'ProductImage' upload files nor its upload directories", () => {
+        try {
+          const { totalImageDirectories, totalImageSubdirectories, totalImageFiles } = getImageUploadData("product_images");
+          expect(totalImageDirectories).to.equal(numberOfProductImageDirectories);
+          expect(totalImageSubdirectories).to.equal(numberOfProductImageSubDirectories);
+          expect(totalImageFiles).to.equal(numberOfProductImageFiles);
+        } catch (error) {
+          throw error;
+        }
+      });
+    });
+    // END TEST GET GET_MANY logged in no account //
+
+    // TEST GET_ONE action logged in no account //
+    describe("GET '/api/products/:productId' - LOGGED IN no ACCOUNT - tests with 'ProductImages' - GET_MANY action", () => {
+    
+      it("Should NOT fetch 'Product' models and return a correct error response", (done) => {
+        chai.request(server)
+          .get("/api/products/" + (firstAdminsProduct._id as Types.ObjectId))
+          .set({ "Authorization": thirdAdminsToken })
+          .end((err, res) => {
+            if (err) done(err);
+            // assert correct response //
+            expect(res.status).of.equal(401);
+            expect(res.body.responseMsg).be.a("string");
+            expect(res.body.error).to.be.an("object");
+            expect(res.body.errorMessages).to.be.an("array");
+            expect(res.body.product).to.be.undefined;
+            done();
+          });
+      });
+      it("Should NOT remove any 'ProductImage' models from the queried 'Product' model", (done) => {
+        ProductImage.find({ productId: firstAdminsProduct._id }).exec()
+          .then((products) => {
+            expect(products.length).to.equal(queriedProductImagesTotal);
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should NOT alter anything within 'ProductImage' models in the database", (done) => {
+        ProductImage.countDocuments().exec()
+          .then((number) => {
+            expect(number).to.equal(totalProductImages);
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should NOT alter any 'ProductImage' upload files nor its upload directories", () => {
+        try {
+          const { totalImageDirectories, totalImageSubdirectories, totalImageFiles } = getImageUploadData("product_images");
+          expect(totalImageDirectories).to.equal(numberOfProductImageDirectories);
+          expect(totalImageSubdirectories).to.equal(numberOfProductImageSubDirectories);
+          expect(totalImageFiles).to.equal(numberOfProductImageFiles);
+        } catch (error) {
+          throw error;
+        }
+      });
+
+    });
+    // END TEST GET_ONE action logged in no account //
+
+    // TEST POST CREATE logged in no account //
+    describe("POST '/api/products/create' - LOGGED IN no ACCOUNT - tests with 'ProductImages' - CREATE action", () => {
+
+      it("Should NOT create a 'Product' model and return a correct error response", (done) => {
+        const mockProduct = generateMockProductData(1);
+        chai.request(server)
+          .post("/api/products/create")
+          .set({ "Authorization": thirdAdminsToken })
+          .send( ...mockProduct )
+          .end((err, res) => {
+            if (err) done(err);
+            // assert correct response //
+            expect(res.status).of.equal(401);
+            expect(res.body.responseMsg).be.a("string");
+            expect(res.body.error).to.be.an("object");
+            expect(res.body.errorMessages).to.be.an("array");
+            expect(res.body.newProduct).to.be.undefined;
+            done();
+          });
+      });
+      it("Should NOT alter anything within 'ProductImage' models in the database", (done) => {
+        ProductImage.countDocuments().exec()
+          .then((number) => {
+            expect(number).to.equal(totalProductImages);
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should NOT alter any 'ProductImage' upload files nor its upload directories", () => {
+        try {
+          const { totalImageDirectories, totalImageSubdirectories, totalImageFiles } = getImageUploadData("product_images");
+          expect(totalImageDirectories).to.equal(numberOfProductImageDirectories);
+          expect(totalImageSubdirectories).to.equal(numberOfProductImageSubDirectories);
+          expect(totalImageFiles).to.equal(numberOfProductImageFiles);
+        } catch (error) {
+          throw error;
+        }
+      });
+
+    });
+    // END TEST POST CREATE action logged in no account //
+
+    // TEST PATCH EDIT  action logged in no account //
+    describe("PATCH '/api/products/update/:productId' - LOGGED IN no ACCOUNT - tests with 'ProductImages' - EDIT action", () => {
+
+      it("Should NOT update 'Product' model and return a correct response", (done) => {
+        const mockProduct = generateMockProductData(1);
+        chai.request(server)
+          .patch("/api/products/update/" + (firstAdminsProduct._id as Types.ObjectId))
+          .set({ "Authorization": thirdAdminsToken })
+          .send(...mockProduct)
+          .end((err, res) => {
+            if (err) done(err);
+            // assert correct response //
+            expect(res.status).of.equal(401);
+            expect(res.body.responseMsg).be.a("string");
+            expect(res.body.error).to.be.an("object");
+            expect(res.body.errorMessages).to.be.an("array");
+            expect(res.body.editedProduct).to.be.undefined;
+            done();
+          });
+      });
+      it("Should NOT remove any 'ProductImage' models from the queried 'Product' model", (done) => {
+        ProductImage.find({ productId: firstAdminsProduct._id }).exec()
+          .then((products) => {
+            expect(products.length).to.equal(queriedProductImagesTotal);
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should NOT alter anything within 'ProductImage' models in the database", (done) => {
+        ProductImage.countDocuments().exec()
+          .then((number) => {
+            expect(number).to.equal(totalProductImages);
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should NOT alter any 'ProductImage' upload files nor its upload directories", () => {
+        try {
+          const { totalImageDirectories, totalImageSubdirectories, totalImageFiles } = getImageUploadData("product_images");
+          expect(totalImageDirectories).to.equal(numberOfProductImageDirectories);
+          expect(totalImageSubdirectories).to.equal(numberOfProductImageSubDirectories);
+          expect(totalImageFiles).to.equal(numberOfProductImageFiles);
+        } catch (error) {
+          throw error;
+        }
+      });
+
+    });
+    // END TEST PATCH EDIT action logged in no account //
+
+    // TEST DELETE DELETE action logged in no account //
+    describe("DELETE '/api/products/delete/:productId' - LOGGED IN no ACCOUNT - tests with 'ProductImages' - DELETE action", () => {
+    
+      it("Should NOT delete the 'Product' model and return a correct response", (done) => {
+        chai.request(server)
+          .delete("/api/products/delete/" + String(firstAdminsProduct._id))
+          .set({ "Authorization": thirdAdminsToken })
+          .end((err, res) => {
+            if (err) done(err);
+            // assert correct response //
+            expect(res.status).of.equal(401);
+            expect(res.body.responseMsg).be.a("string");
+            expect(res.body.error).to.be.an("object");
+            expect(res.body.errorMessages).to.be.an("array");
+            expect(res.body.deletedProduct).to.be.undefined;
+            done();
+          });
+      });
+      it("Should NOT remove any 'ProductImage' models from the queried 'Product' model", (done) => {
+        ProductImage.find({ productId: firstAdminsProduct._id }).exec()
+          .then((products) => {
+            expect(products.length).to.equal(queriedProductImagesTotal);
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should NOT alter anything within 'ProductImage' models in the database", (done) => {
+        ProductImage.countDocuments().exec()
+          .then((number) => {
+            expect(number).to.equal(totalProductImages);
+            done();
+          })
+          .catch((error) => {
+            done(error);
+          });
+      });
+      it("Should NOT alter any 'ProductImage' upload files nor its upload directories", () => {
+        try {
+          const { totalImageDirectories, totalImageSubdirectories, totalImageFiles } = getImageUploadData("product_images");
+          expect(totalImageDirectories).to.equal(numberOfProductImageDirectories);
+          expect(totalImageSubdirectories).to.equal(numberOfProductImageSubDirectories);
+          expect(totalImageFiles).to.equal(numberOfProductImageFiles);
+        } catch (error) {
+          throw error;
+        }
+      });
+
+    });
+    // END TEST DELETE DELETE action correct BusinessAccount //
+    
+  });
+  // END TEST CONTEXT Admin is logged in no account 'ProductsController' actions tests //
+
 });
